5ff9896549b81e53f09ef2a173f431da9f959682
Mehdi Hamime <mhamime@cloud.deloitte.fr>
2020-06-10 16:11:47 +0100

    analytics added

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,25 @@
 import Head from 'next/head'
 
+import Document, { Html, Main, NextScript } from "next/document"
+
 
 export default function Header() {
     return (   
     <Head>
         <title> مناطق التخفيف من تدابير الحجر الصحي بالمملكة المغربية </title>
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EE2X0VV9FM"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
        
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-EE2X0VV9FM"></script>
-            <script>
+   
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
             
               gtag('config', 'G-EE2X0VV9FM');
-            </script>
+      
               `,
           }}
         />
