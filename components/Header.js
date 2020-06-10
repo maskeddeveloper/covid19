02bf7b0dea85b1e82c8b56f99c134ece95ce5ab7
Mehdi Hamime <mhamime@cloud.deloitte.fr>
2020-06-10 16:02:53 +0100

    analytics added

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,21 @@ export default function Header() {
     <Head>
         <title> مناطق التخفيف من تدابير الحجر الصحي بالمملكة المغربية </title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-EE2X0VV9FM"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-EE2X0VV9FM');
+            </script>
+              `,
+          }}
+        />
       </Head>)
 
  }
